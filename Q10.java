aa0ccecb293390ba468f444efa204b8cd62986bc
Minal Rai <minalrai1235@gmail.com>
2020-10-07 12:03:50 +0545

    Update Q10.java

--- Q10.java
+++ Q10.java
@@ -1,4 +1,6 @@
+//The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
 
+//Find the sum of all the primes below two million.
 public class Q10 {
 	public static void main(String[] args) {
 		int i,j,flag=0;
@@ -23,3 +25,5 @@ public class Q10 {
 	}
 
 }
+//output
+//142913828922
