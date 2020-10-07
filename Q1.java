deb17454bad3d1eb777189dcb7279062a9cacdcb
Minal Rai <minalrai1235@gmail.com>
2020-10-07 12:31:40 +0545

    Update Q1.java

--- Q1.java
+++ Q1.java
@@ -6,7 +6,7 @@ class Q1 {
 
 	public static void main(String[] args) {
 		int sum=0;
-		for(int i=1;i<=1000;i++)
+		for(int i=1;i<1000;i++)
 		{
 			if((i%3==0)||(i%5==0))
 			{
@@ -21,4 +21,4 @@ class Q1 {
 
 }
 //output
-//234168
+//233168
