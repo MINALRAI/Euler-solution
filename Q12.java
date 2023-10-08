c88765bdcd46afd0e19deb0c7d0507b10703396f
Vishnugupta21 <vgupta1408301@gmail.com>
2023-10-08 21:01:17 +0530

    vishnu'scommit

--- Q12.java
+++ Q12.java
@@ -11,6 +11,13 @@
 //15: 1,3,5,15
 //21: 1,3,7,21
 //28: 1,2,4,7,14,28
+
+
+
+// heloo worold 
+// hello himanshu 
+//
+
 //We can see that 28 is the first triangle number to have over five divisors.
 
 //What is the value of the first triangle number to have over five hundred divisors?
